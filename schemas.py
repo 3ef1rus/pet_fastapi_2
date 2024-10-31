891b9809bc3683c4c425cac8b0316f311c691b14
3ef1rus <ivanvyal321@gmail.com>
2024-10-31 18:36:04 +0300

    reposition classes

--- schemas.py
+++ schemas.py
@@ -1,28 +1,11 @@
-from typing import Optional, Annotated
+from typing import Optional, Annotated, Union
 
 from pydantic import BaseModel, Field
-
-
-class WorkBase(BaseModel):
-    name: Annotated[str, Field(..., min_length=2, title="Название работы")]
-    salary: Annotated[int, Field(..., ge=0, title="Размер зарплаты")]
-    user_id:int
-
-class WorkResponse(WorkBase):
-    id: int
-
-    class Config:
-        from_attributes = True
-
-
-class WorkCreate(BaseModel):
-    pass
 
 
 class UserBase(BaseModel):
     name: str
     age: int
-    works: Optional[int] = "unemployed"
 
 
 class User(UserBase):
@@ -34,5 +17,25 @@ class User(UserBase):
 
 class UserCreate(UserBase):
     pass
+
+
+class WorkBase(BaseModel):
+    name: Annotated[str, Field(..., min_length=2, title="Название работы")]
+    salary: Annotated[int, Field(..., ge=0, title="Размер зарплаты")]
+    user_id:int
+
+
+class Work(WorkBase):
+    id: int
+    user:User
+
+    class Config:
+        from_attributes = True
+
+
+class WorkCreate(WorkBase):
+    pass
+
+
 
 
