3289799afdf962d63244f2c51ebd614db9c1ad19
3ef1rus <ivanvyal321@gmail.com>
2024-11-02 20:43:07 +0300

    change connection one to many

--- models.py
+++ models.py
@@ -1,22 +1,21 @@
-from sqlalchemy import Column,Integer,String,ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
 
-class User(Base):
+class UserDB(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True,index=True)
-    name = Column(String,index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
     age = Column(Integer)
+    work_id = Column(Integer, ForeignKey("works.id"))
+    work = relationship("WorkDB", backref="users")
 
 
-class Work(Base):
+class WorkDB(Base):
     __tablename__ = "works"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     salary = Column(Integer)
-    user_id=Column(Integer,ForeignKey('users.id'))
-
-    user = relationship("User")
